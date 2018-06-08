a39a5814d1ec70e1089d4fdeb0f8304139e5ef1a
toywonder <toy.wonder70@gmail.com>
2018-06-08 19:00:55 +0900

    initial commit

---
+++ db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180608094428) do
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "prefecture"
+    t.string "city"
+    t.string "town"
+    t.integer "tyo"
+    t.integer "ban"
+    t.integer "go"
+    t.text "access"
+    t.integer "tel"
+    t.time "weekday_start"
+    t.time "weekday_finish"
+    t.time "saturday_start"
+    t.time "saturday_finish"
+    t.time "sunday_start"
+    t.time "sunday_finish"
+    t.string "hp"
+    t.boolean "wifi"
+    t.boolean "socket"
+    t.boolean "smoking"
+    t.integer "mainstore_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mainstore_id"], name: "index_stores_on_mainstore_id"
+  end
+
+end
