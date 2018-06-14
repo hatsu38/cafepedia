9909349a69625bf8fa7a646ce3b9edb6e391ec92
toywonder <toy.wonder70@gmail.com>
2018-06-14 14:25:06 +0900

    検索結果にfilterを含まないように変更

--- app/javascript/packs/store.js
+++ app/javascript/packs/store.js
@@ -34,39 +34,40 @@ var app = new Vue({
       app.refreshFilter();
     },
     refreshFilter: function(){
-      var stores = [];
       if(app.isSocketFilterOn && app.isWifiFilterOn){
-        var stores = app.allStores.filter(function(value){
+       var stores_list = app.stores.filter(function(value){
           return value.socket && value.wifi
         });
       }else if(app.isSocketFilterOn && !app.isWifiFilterOn){
-        var stores = app.allStores.filter(function(value){
+        var stores_list = app.stores.filter(function(value){
           return value.socket
         });
       }else if(!app.isSocketFilterOn && app.isWifiFilterOn){
-        var stores = app.allStores.filter(function(value){
+        var stores_list = app.stores.filter(function(value){
           return value.wifi
         });
       }else if(!app.isSocketFilterOn && !app.isWifiFilterOn){
-        var stores = app.allStores
+        var stores_list = app.stores
       }else{
         alart("Error!");
       }
-      app.stores = stores
+      app.stores = stores_list
     },
     filterStores: function(){
-      var stores = [];
+      // var stores = [];
       var searchWord = this.storeSearch && this.storeSearch.toLowerCase();
       console.log(searchWord);
       if(searchWord){
-        stores = app.allStores.filter(function(value){
+        var stores_list = app.allStores.filter(function(value){
           return Object.keys(value).some(function(key){
             if(key === 'name'){
             return String(value[key]).toLowerCase().indexOf(searchWord) > -1
             }
           })
         })
-        return app.stores = stores
+        app.isSocketFilterOn = false
+        app.isWifiFilterOn = false
+        return app.stores = stores_list
       }
       return app.stores = app.allStores
     }
