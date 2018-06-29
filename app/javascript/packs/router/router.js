8264322ec4075adebcbcd96f0be67c7ca9ac307a
toywonder <toy.wonder70@gmail.com>
2018-06-29 14:51:37 +0900

    header.vue削除完了

--- app/javascript/packs/router/router.js
+++ app/javascript/packs/router/router.js
@@ -2,7 +2,6 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Index from '../components/index.vue'
 import Show from '../components/show.vue'
-import Header from '../components/header.vue'
 
 export default new VueRouter({
   mode: 'history',
