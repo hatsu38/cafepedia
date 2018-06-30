7d467e10d4cfa128e31a959701b0e051034e0076
toywonder <toy.wonder70@gmail.com>
2018-06-30 11:59:41 +0900

    gmapがloadのタイミングで表示

--- app/javascript/packs/components/show.vue
+++ app/javascript/packs/components/show.vue
@@ -34,14 +34,24 @@ export default {
       });
     },
     gmapcreate: function(){
-        console.log(this.store);
-        var mapOptions = {"center":{"lat": parseFloat(this.store.lat),"lng": parseFloat(this.store.lng)},"mapTypeId": "roadmap","zoom": 15};
-        var mapElement = document.getElementById('map');
-        var map = new google.maps.Map(mapElement, mapOptions);
-        google.maps.event.addDomListener(window, "resize", function() { var center = map.getCenter(); google.maps.event.trigger(map, "resize"); map.setCenter(center); });
+      var lat = parseFloat(this.store.lat);
+      var lng = parseFloat(this.store.lng);
+      var store_position = {"lat": lat,"lng": lng}
+      var mapOptions = {"center": store_position,"mapTypeId": "roadmap","zoom": 16};
+      var mapElement = document.getElementById('map');
+      var map = new google.maps.Map(mapElement, mapOptions);
+      google.maps.event.addDomListener(window, "resize", function() { var center = map.getCenter(); google.maps.event.trigger(map, "resize"); map.setCenter(center); });
+      this.gmarkerDesp(lat,lng,map);
     },
     gmapdesp: function(){
       google.maps.event.addDomListener(window, 'load', this.gmapcreate);
+    },
+    gmarkerDesp: function(lat,lng,map){
+      var marker = new google.maps.Marker({
+        // マーカーを置く緯度経度
+        position: new google.maps.LatLng(lat,lng),
+        map: map
+      });
     }
   }
 }
