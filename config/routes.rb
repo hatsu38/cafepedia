6de63ed2ff82b4f68cd0b05d63ea444755a420f2
toywonder <toy.wonder70@gmail.com>
2019-04-07 18:22:31 +0900

    :sparkles:deviseを追加して動作確認まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'stores/index'
   post 'stores/contact'
   post 'stores/leak'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180807121327) do
+ActiveRecord::Schema.define(version: 20190407075055) do
 
   create_table "inquiries", force: :cascade do |t|
     t.string "email"
@@ -58,6 +58,18 @@ ActiveRecord::Schema.define(version: 20180807121327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mainstore_id"], name: "index_stores_on_mainstore_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
