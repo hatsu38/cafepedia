4fb9452ddcf5de1bd05ba3295e3964057bf4e6b2
toywonder <toy.wonder70@gmail.com>
2019-04-07 21:37:42 +0900

    :hocho:devise機能をログインとログアウトのみに制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def after_sign_in_path_for(resource)
+    admin_index_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
+
 end
