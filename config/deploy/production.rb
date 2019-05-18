dbc1faf0031092df4194c6a44635e206a8d3b8af
toywonder <toy.wonder70@gmail.com>
2019-05-18 20:47:25 +0900

    :sparkles:本番環境にAdsense追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 # AWS-TEST環境
-server '54.64.51.15', user: 'hajime', roles: %w{app db web}
-set :ssh_options, keys: '~/.ssh/cafepedia-test_key_rsa'
+# server '54.64.51.15', user: 'hajime', roles: %w{app db web}
+# set :ssh_options, keys: '~/.ssh/cafepedia-test_key_rsa'
 
 # AWS-本番環境
-# server '13.113.90.206', user: 'hajime', roles: %w{app db web}
-# set :ssh_options, keys: '~/.ssh/cafepedia_rsa'
+server '13.113.90.206', user: 'hajime', roles: %w{app db web}
+set :ssh_options, keys: '~/.ssh/cafepedia_rsa'
